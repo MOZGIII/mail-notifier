6b809e259f1673f4fe3b8b5b2f66397da48974e4
MOZGIII <mike-n@narod.ru>
2026-01-18 20:07:07 +0400

    Add color_eyre

--- crates/bin/main/src/main.rs
+++ crates/bin/main/src/main.rs
@@ -1,8 +1,11 @@
 //! Main entrypoint.
 
 #[tokio::main]
-async fn main() {
+async fn main() -> color_eyre::eyre::Result<()> {
+    color_eyre::install()?;
     tracing_subscriber::fmt::init();
 
     tracing::info!("Hello, world!");
+
+    Ok(())
 }
