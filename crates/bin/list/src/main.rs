aa7a2c8b5a55d28d72f5eb31ecc9c70279bd4849
MOZGIII <mike-n@narod.ru>
2026-01-28 04:53:01 +0400

    Refactor the config code placement and logic (#48)

--- crates/bin/list/src/main.rs
+++ crates/bin/list/src/main.rs
@@ -10,44 +10,27 @@ async fn main() -> color_eyre::eyre::Result<()> {
 
     let config = config_load::with_default_env_var().await?;
     let _keyring_guard = config_bringup::init_keyring_if_needed(&config)?;
+    let servers = config_bringup::servers_only(&config).await?;
+    drop(config);
 
-    for server in &config.servers {
-        let server_config = config_bringup::bringup_server_config(server).await?;
-
+    for server in &servers {
         tracing::info!(
-            server_name = %server_config.server_name,
-            imap_host = %server_config.host,
-            imap_port = server_config.port,
-            imap_tls_mode = ?server_config.tls_mode,
+            server_name = %server.server_name,
+            imap_host = %server.host,
+            imap_port = server.port,
+            imap_tls_mode = ?server.tls_mode,
             "listing IMAP mailboxes"
         );
 
-        let auth = match &server_config.auth {
-            imap_monitor::config::Auth::Login { username, password } => {
-                imap_session::auth::Params::Login { username, password }
-            }
-            imap_monitor::config::Auth::OAuth2Credentials { user, access_token } => {
-                imap_session::auth::Params::OAuth2 { user, access_token }
-            }
-        };
-
-        let mut session = imap_session::setup(imap_session::SetupParams {
-            host: &server_config.host,
-            port: server_config.port,
-            tls_mode: server_config.tls_mode,
-            tls_server_name: &server_config.tls_server_name,
-            auth,
-        })
-        .await?;
+        let mut session = imap_session::setup(server.to_imap_session_params()).await?;
 
         let mut list_stream = session.list(None, Some("*")).await?;
-        println!("{}:", server_config.server_name);
+        println!("{}:", server.server_name);
         while let Some(name) = list_stream.try_next().await? {
             let name = imap_utf7::ImapUtf7Str::new(name.name())?;
             println!("  {name}");
         }
     }
-    drop(config);
 
     Ok(())
 }
